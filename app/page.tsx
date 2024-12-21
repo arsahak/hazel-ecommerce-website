c5dd5959f97d725789b0cba62425da00b859acae
arsahak <arsahak99@gmail.com>
2024-12-21 23:31:40 +0600

    update favicon and og image

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,20 @@ import JointheClub from "@/components/home/JointheClub";
 import PackagingEveryDetails from "@/components/home/PackagingEveryDetails";
 import TimelessElegant from "@/components/home/TimelessElegant";
 
+export const metadata = {
+  title: "Hazel Fashion Store",
+  description: "B2B and B2C eCommerce platform",
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-USA",
+    },
+  },
+  openGraph: {
+    images: "/opengraph-image.jpg",
+  },
+};
+
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
